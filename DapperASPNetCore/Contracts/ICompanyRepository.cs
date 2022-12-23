f0d393f183ba8549e17641d54458e473047c2e85
pri.tom78 <pritompal789@gmail.com>
2022-12-24 01:48:22 +0600

    first commit

---
+++ DapperASPNetCore/Contracts/ICompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using DapperASPNetCore.Dto;
+using DapperASPNetCore.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DapperASPNetCore.Contracts
+{
+    public interface ICompanyRepository
+    {
+        public Task<IEnumerable<Company>> GetCompanies();
+        public Task<Company> GetCompany(int id);
+        public Task<Company> CreateCompany(CompanyForCreationDto company);
+        public Task UpdateCompany(int id, CompanyForUpdateDto company);
+        public Task DeleteCompany(int id);
+
+        public Task<Company> GetCompanyByEmployeeId(int id);
+        public Task<Company> GetCompanyEmployeesMultipleResults(int id);
+        public Task<List<Company>> GetCompaniesEmployeesMultipleMapping();
+        Task CreateMultipleCompanies(List<CompanyForCreationDto> companies);
+    }
+}
